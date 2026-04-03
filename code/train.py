09cbd626ad895a850532ec065a87ad52fd31648c
Kaemri <lordlag.lager@gmail.com>
2026-04-03 22:53:43 +0300

    full training code

--- code/train.py
+++ code/train.py
@@ -1,1 +1,16 @@
+from ultralytics import YOLO
 
+model = YOLO("yolo26m.pt") # yolo26n-s-m-l-x.pt
+# train models
+train = model.train(
+    data='dataset.yaml',
+    epochs=100,
+    imgsz=640,
+    optimizer='MuSGD',
+    batch=0.80,
+    seed=226,
+    name='medium', # nano, small, medium, large, xlarge
+    project='train',
+    exist_ok=True,
+    device=0
+)
