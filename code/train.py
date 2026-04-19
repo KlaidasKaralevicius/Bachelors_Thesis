326cd29286f3275646abc350ea69e57a3d4ce129
Kaemri <lordlag.lager@gmail.com>
2026-04-20 01:02:58 +0300

    export weight to edge

--- code/train.py
+++ code/train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("./weights/yolo26x.pt") # yolo26n-s-m-l-x.pt
+model = YOLO("./weights/yolo26n-trained.pt") # yolo26n-s-m-l-x.pt
 # train models
 train = model.train(
     data='dataset.yaml',
@@ -8,10 +8,12 @@ train = model.train(
     imgsz=640,
     freeze=10,
     optimizer='MuSGD',
-    batch=12,
+    batch=0.8,
     seed=226,
     name='xlarge', # nano, small, medium, large, xlarge
     project='train',
     exist_ok=True,
     device=0
 )
+# export ncnn for edge deployment
+model.export(format='ncnn')
